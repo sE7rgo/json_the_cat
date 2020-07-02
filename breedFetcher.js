de638c40d8df248316a6112fa82cd8b9bb8896d4
sE7rgo <se7rgo@gmail.com>
2020-07-02 22:10:21 +0000

    index,test,funkFile

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,20 @@
 const request = require('request');
-const breeds = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breeds}`, function(error, response, body) {
-  if (error) console.error('error:', error);
-  const data = JSON.parse(body);
-  if (!data[0]) {
-    console.log('not existing breed yet :)\ntry one more time');
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      callback(error);
+    } else {
+      const data = JSON.parse(body);
+      if (!data[0]) {
+        const error = 'not invented yet:)';
+        callback(error);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
+
+module.exports = fetchBreedDescription;
